d0bdb01427ed472b5c811ae4380417a413ea87a9
Rakhee <rsmistry05061996@gmail.com>
2026-09-22 21:59:52 +0530

    Rules of find method of javascript

--- Find_Method.js
+++ Find_Method.js
@@ -6,3 +6,8 @@ console.log(result1); // Output: undefined
 // Rule 2: First element satisfies the condition
 const result2 = arr.find(element => element > 3);
 console.log(result2); // Output: 4
+
+// Rule 3: Handle empty slots in the array
+const arrWithEmptySlots = [1, , 3, , 5];
+const result3 = arrWithEmptySlots.find(element => element === undefined);   
+console.log(result3); // Output: undefined
